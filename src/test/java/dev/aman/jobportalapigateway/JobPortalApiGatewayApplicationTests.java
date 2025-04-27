e534380b65f67558e57b9d84f568f19fcbed2bbb
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-27 21:01:36 +0530

    Created API Gateway Repo

---
+++ src/test/java/dev/aman/jobportalapigateway/JobPortalApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportalapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
